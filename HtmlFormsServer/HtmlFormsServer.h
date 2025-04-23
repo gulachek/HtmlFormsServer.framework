24c8c8e64ba7587c65e9a9cf990c25011befae58
Nicholas Gulachek <nsgulachek@gmail.com>
2025-04-23 18:11:48 -0500

    Initial Commit

---
+++ HtmlFormsServer/HtmlFormsServer.h
@@ -0,0 +1,18 @@
+//
+//  HtmlFormsServer.h
+//  HtmlFormsServer
+//
+//  Created by Nicholas Gulachek on 4/23/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HtmlFormsServer.
+FOUNDATION_EXPORT double HtmlFormsServerVersionNumber;
+
+//! Project version string for HtmlFormsServer.
+FOUNDATION_EXPORT const unsigned char HtmlFormsServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HtmlFormsServer/PublicHeader.h>
+
+
---
+++ HtmlFormsServerTests/HtmlFormsServerTests.m
@@ -0,0 +1,36 @@
+//
+//  HtmlFormsServerTests.m
+//  HtmlFormsServerTests
+//
+//  Created by Nicholas Gulachek on 4/23/25.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HtmlFormsServerTests : XCTestCase
+
+@end
+
+@implementation HtmlFormsServerTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
