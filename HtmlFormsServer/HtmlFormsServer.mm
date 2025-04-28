43e62f59aff684cea2f0609c49a61815db5ea57f
Nicholas Gulachek <nsgulachek@gmail.com>
2025-04-27 22:09:10 -0500

    Add logging

--- HtmlFormsServer/HtmlFormsServer.mm
+++ HtmlFormsServer/HtmlFormsServer.mm
@@ -45,18 +45,24 @@ static void evt_callback(const html_forms_server_event *evt, void *ctx) {
 }
 
 -(void) start{
-    if (self->is_running_)
+    if (self->is_running_) {
+        os_log_debug(self->log_, "Skipping server start because it is already running");
         return;
+    }
     
+    os_log_info(self->log_, "Starting server");
     self->is_running_ = YES;
     html_forms_server_set_event_callback(self->server_, evt_callback, (__bridge void*)self);
     html_forms_server_run(self->server_);
 }
 
 -(void) stop{
-    if (!self->is_running_)
+    if (!self->is_running_) {
+        os_log_debug(self->log_, "Skipping server stop because it is already stopped");
         return;
+    }
     
+    os_log_info(self->log_, "Stopping server");
     self->is_running_ = NO;
     html_forms_server_set_event_callback(self->server_, nil, nil);
     html_forms_server_stop(self->server_);
@@ -64,20 +70,33 @@ static void evt_callback(const html_forms_server_event *evt, void *ctx) {
 
 -(void) handleEvent:(const html_forms_server_event *)evt {
     id<HtmlFormsServerDelegate> del = self.delegate;
-    if (!del)
-        return;
     
     if (evt->type == HTML_FORMS_SERVER_EVENT_OPEN_URL) {
         NSInteger winId = evt->data.open_url.window_id;
         NSString *urlStr = [NSString stringWithCString:evt->data.open_url.url encoding:NSUTF8StringEncoding];
         NSURL *url = [NSURL URLWithString:urlStr];
         
-        [del openUrl:url window:winId];
+        if (os_log_debug_enabled(self->log_)) {
+            os_log_debug(self->log_, "Navigating window %ld to url=%@", (long)winId, url);
+        }
+        
+        if (del)
+            [del openUrl:url window:winId];
     } else if (evt->type == HTML_FORMS_SERVER_EVENT_CLOSE_WINDOW) {
         NSInteger winId = evt->data.close_win.window_id;
-        [del closeWindow:winId];
+        
+        if (os_log_debug_enabled(self->log_)) {
+            os_log_debug(self->log_, "Closing window %ld", (long)winId);
+        }
+        
+        if (del)
+            [del closeWindow:winId];
     } else {
         os_log(self->log_, "Received server event with unknown type (type=%d)", evt->type);
+    }
+    
+    if (!del) {
+        os_log(self->log_, "No HtmlFormsServerDelegate is set to handle event");
     }
 }
 
